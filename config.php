294059251dc69aa3762c202c3be66046629ede2d
Voula Troulaki <v.trulaki@usableweb.gr>
2013-12-03 12:35:14 +0200

    First Commit

---
+++ config.php
@@ -0,0 +1,13 @@
+<!-- Warning this page Charset must be UTF-8-->
+<?php
+	
+
+	
+	
+	$admin_email = "test@gmail.com";		//Place  your Email here
+	$url   = "ok.html"; // Place the url of the page you want to redirect after the form is //submitted successfully
+	$Papaki_apikey="myapikey";//place your apikey here
+	$papaki_Post_url="https://api.papaki.gr/register_url2.aspx";
+	
+	
+?>

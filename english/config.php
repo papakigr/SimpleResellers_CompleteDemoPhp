a8ee271183d4fc86e48ba787aef3793528ceb7ad
Voula Troulaki <resellers@papaki.gr>
2017-06-06 12:45:16 +0300

    api url changes to api.papaki.com

--- english/config.php
+++ english/config.php
@@ -7,7 +7,7 @@
 	$admin_email = "test@gmail.com";		//Place  your Email here
 	$url   = "ok.html"; // Place the url of the page you want to redirect after the form is //submitted successfully
 	$Papaki_apikey="myapikey";//place your apikey here
-	$papaki_Post_url="https://api.papaki.gr/register_url2.aspx";
+	$papaki_Post_url="https://api.papaki.com/register_url2.aspx";
 	
 	
 ?>
